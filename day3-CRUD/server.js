9c05387ee6ac7f9cbcbb53c1f523fdab45775650
Aditya Nath <adityanath271@gmail.com>
2026-08-26 08:18:20 +0530

    CRUD - delete

--- day3-CRUD/server.js
+++ day3-CRUD/server.js
@@ -10,12 +10,23 @@ app.use(express.json());
 app.post("/create", (req, res) => {
     let body = req.body;
     users.push(body);
-    res.send("users saved ");
+    res.send("Users saved succesfullly");
 });
 
 //Get - Read
 app.get("/", (req, res) => {
     res.send(users);
+});
+
+//delete
+app.delete("/delete/:id", (req, res) => {
+    let { id } = req.params;
+    
+    let usersdata = users.filter((val) => val.id !== id)
+    console.log(usersdata)
+    users = usersdata;
+    res.send(usersdata)
+
 });
 
 app.listen(3000, () => {
