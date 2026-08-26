4b9cb5cd4c48e53508a72d1711610d64912bf9f9
Aditya Nath <adityanath271@gmail.com>
2026-08-26 08:41:55 +0530

    CRUD - Update

--- day3-CRUD/server.js
+++ day3-CRUD/server.js
@@ -8,27 +8,36 @@ app.use(express.json());
 
 //create
 app.post("/create", (req, res) => {
-    let body = req.body;
-    users.push(body);
-    res.send("Users saved succesfullly");
+  let body = req.body;
+  users.push(body);
+  res.send("Users saved succesfullly");
 });
 
 //Get - Read
 app.get("/", (req, res) => {
-    res.send(users);
+  res.send(users);
+});
+//update
+app.put("/update/:id", (req, res) => {
+  let { id } = req.params;
+  let { name } = req.body;
+
+  let updatedUser = users.map((val) =>
+    val.id === id ? { ...val, name } : val,
+  );
+  res.send(updatedUser);
 });
 
 //delete
 app.delete("/delete/:id", (req, res) => {
-    let { id } = req.params;
-    
-    let usersdata = users.filter((val) => val.id !== id)
-    console.log(usersdata)
-    users = usersdata;
-    res.send(usersdata)
+  let { id } = req.params;
 
+  let usersdata = users.filter((val) => val.id !== id);
+  console.log(usersdata);
+  users = usersdata;
+  res.send(usersdata);
 });
 
 app.listen(3000, () => {
-    console.log("server is working");
+  console.log("server is working");
 });
