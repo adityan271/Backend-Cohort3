888a316a4fa06158bb5348279d0751524180cfb4
Aditya Nath <adityanath271@gmail.com>
2026-08-29 21:05:45 +0530

    Mongoose used and connection made

--- day4-mongoDB/server.js
+++ day4-mongoDB/server.js
@@ -1,11 +1,27 @@
 const express = require("express");
+//mongoose
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("Ok i'm chalu")
-})
+//mongoose - connection
+const connectDb = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://adityanath271_db_user:coho@cohort-cluster.fhlabqb.mongodb.net/",
+    );
 
+    console.log("MongoDb connected");
+  } catch (error) {
+    console.log("Error while connecting to database", error);
+  }
+};
+
+connectDb();
+
+app.get("/", (req, res) => {
+  res.send("Ok i'm chalu");
+});
 
 app.listen(3000, () => {
   console.log("Server is working");
