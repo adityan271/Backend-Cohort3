0a337631e6fd934e0a8417dd0630c44cabf85851
Aditya Nath <adityanath271@gmail.com>
2026-09-19 20:31:57 +0530

    basic form made with three input fields

--- day9-practise-formData/client/src/App.jsx
+++ day9-practise-formData/client/src/App.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const App = () => {
+  const { register, handleSubmit } = useForm();
+
+  const submitHandler = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <h1>Helooo buddyyy</h1>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <input
+          {...register("name")}
+          type="text"
+          placeholder="Enter your name"
+        />
+        <br />
+        <br />
+        <input
+          {...register("email")}
+          type="text"
+          placeholder="Enter your email"
+        />
+        <br />
+        <br />
+        <input
+          {...register("profile_pic")}
+          type="file"
+          placeholder="Upload your profile pic"
+        />
+        <br />
+        <br />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
