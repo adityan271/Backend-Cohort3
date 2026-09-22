34a3e6824ab264afaba7d5254df06e12517507b1
Aditya Nath <adityanath271@gmail.com>
2026-09-22 19:28:37 +0530

    multer setup

--- day10-image-kit/server/src/routes/post.route.js
+++ day10-image-kit/server/src/routes/post.route.js
@@ -1,6 +1,7 @@
 import express from "express";
-const router = express.Router()
+import { upload } from "../config/multer.config";
+const router = express.Router();
 
-router.post("/create",createPost)
+router.post("/create", upload.single("image"), createPost);
 
-export default router
+export default router;
