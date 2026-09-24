23c0792a84d30a2371507697ce30cc7269d6e1d8
Aditya Nath <adityanath271@gmail.com>
2026-09-24 09:23:45 +0530

    Token made for Authentatication

--- day11-Authentication-1/src/app/app.js
+++ day11-Authentication-1/src/app/app.js
@@ -1,9 +1,36 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
+//Middleware - express.json() reads the json data from frontend
+app.use(express.json());
+
 app.get("/api", (req, res) => {
-  res.status(200).json({ message: "Welcome to the authentication API" });
+    res.status(200).json({ message: "Welcome to the authentication API" });
+});
+
+app.post("/api/register", (req, res) => {
+    const { name, email, password } = req.body;
+
+    const token = jwt.sign(
+        {
+            email,
+            name,
+        },
+        "b79c2bc076e74b15a0742233baf443c7ca9961a26bb50c5660f1fb0db317d829",
+    );
+
+    res.status(201).json({
+        message: "user created successfully",
+        data: {
+            user: {
+                email,
+                name,
+            },
+            token,
+        },
+    });
 });
 
 export default app;
